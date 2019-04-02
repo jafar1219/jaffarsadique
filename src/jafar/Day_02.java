422a78a1a76c83b78f2c0e0abf7309e124f39d21
jaffarsadique <jaffy71@gmail.com>
2019-04-02 15:54:58 +0530

    array

---
+++ src/jafar/Day_02.java
@@ -0,0 +1,14 @@
+package jafar;
+
+public class Day_02 {
+
+	public static void main(String[] args) {
+		int a=21;
+		if (a>=18) {
+			System.out.println("eligible to vote");
+		} else {
+System.out.println("not eligible");
+		}
+	}
+
+}
